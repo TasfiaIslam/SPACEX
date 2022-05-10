14644c4bbac7feb0d0a384adcaef6de93c15c9ba
TasfiaIslam <tasf1a027@gmail.com>
2022-05-10 17:21:44 +0600

    separate component for launch

--- src/features/launches/Launches.tsx
+++ src/features/launches/Launches.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getLaunches, selectLaunches } from './launchSlice';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Launch from './launch';
 
 const Launches = (): JSX.Element => {
   const DESCRIPTION_LIMIT = 80;
@@ -23,32 +24,15 @@ const Launches = (): JSX.Element => {
           {launches?.map((launch, index) => (
             <Col className="col-12 col-sm-6 col-md-4 col-xl-3 gx-5" key={index}>
               <Row className="my-4">
-                <Card className="text-white bg-dark">
-                  <Card.Header className="font-weight-bold text-danger fs-6">
-                    Flight no: {launch?.flight_number}
-                  </Card.Header>
-                  <Card.Body className="py-2">
-                    <Card.Img variant="top" src={launch?.links?.mission_patch} />
-
-                    <Card.Title className="font-weight-bold mt-2 text-warning">
-                      {launch?.mission_name}
-                    </Card.Title>
-                    <Card.Text className="fs-6 font-weight-light">
-                      <p className="" style={{ height: '80px' }}>
-                        {launch?.details?.length < DESCRIPTION_LIMIT
-                          ? launch?.details
-                          : `${launch?.details?.substring(0, DESCRIPTION_LIMIT)} ...`}
-                      </p>
-                      <p className="text-danger">Launched At {launch?.launch_year}</p>
-                      <Col className="pb-4">
-                        <span className=" text-warning">Rocket: </span>
-                        <span className=" text-warning">
-                          {launch?.rocket?.rocket_name}-{launch?.rocket?.rocket_type}
-                        </span>
-                      </Col>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
+                <Launch
+                  flight_number={launch?.flight_number}
+                  mission_patch_img={launch?.mission_patch}
+                  mission_name={launch?.mission_name}
+                  details={launch?.details}
+                  launch_year={launch?.launch_year}
+                  rocket_name={launch?.rocket?.rocket_name}
+                  rocket_type={launch?.rocket?.rocket_type}
+                />
               </Row>
             </Col>
           ))}
---
+++ src/features/launches/launch/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DESCRIPTION_LIMIT = 80;
+
+interface Props {
+  flight_number?: string;
+  mission_patch_img?: string;
+  mission_name?: string;
+  details?: string;
+  launch_year?: string;
+  rocket_name?: string;
+  rocket_type?: string;
+}
+
+const Launch = ({
+  flight_number,
+  mission_patch_img,
+  mission_name,
+  details,
+  launch_year,
+  rocket_name,
+  rocket_type,
+}: Props): JSX.Element => {
+  return (
+    <Card className="text-white bg-dark">
+      <Card.Header className="font-weight-bold text-danger fs-6">
+        Flight no: {!!flight_number && flight_number}
+      </Card.Header>
+      <Card.Body className="py-2">
+        <Card.Img variant="top" src={mission_patch_img ? mission_patch_img : ''} />
+
+        <Card.Title className="font-weight-bold mt-2 text-warning">
+          {!!mission_name && mission_name}
+        </Card.Title>
+        <Card.Text className="fs-6 font-weight-light">
+          <p style={{ height: '80px' }}>
+            {!!details && details?.length < DESCRIPTION_LIMIT
+              ? details
+              : `${details?.substring(0, DESCRIPTION_LIMIT)} ...`}
+          </p>
+          <p className="text-danger">Launched At {!!launch_year && launch_year}</p>
+          <Col className="pb-4">
+            <span className=" text-warning">Rocket: </span>
+            <span className=" text-warning">
+              {!!rocket_name && rocket_name}-{!!rocket_type && rocket_type}
+            </span>
+          </Col>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Launch;
